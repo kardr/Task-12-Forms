a37edfa26da1f73823a984845e21e3fc676f82de
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-29 06:37:35 +0300

    .

--- Task 12 Forms/Form1.Designer.cs
+++ Task 12 Forms/Form1.Designer.cs
@@ -28,13 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(287, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(257, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "ПРЕОБРАЗОВАНИЕ ТИПА string В КЛАСС Regex:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(463, 197);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(224, 59);
+            this.textBox5.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(463, 108);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(224, 59);
+            this.textBox4.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(448, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(257, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "ПРЕОБРАЗОВАНИЕ КЛАССА Regex В ТИП string:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "ОБРАЩЕНИЕ К ЭКЗЕМПЛЯРУ КЛАССА:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 108);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 57);
+            this.textBox3.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(396, 26);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 40);
+            this.textBox2.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(460, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "ШАБЛОН: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "ТЕКСТ:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 40);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(247, 378);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(224, 57);
+            this.textBox6.TabIndex = 31;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(463, 283);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(224, 57);
+            this.textBox7.TabIndex = 32;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(34, 197);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(224, 57);
+            this.textBox8.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(180, 362);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(395, 13);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "ИНДЕКСАТОР, ПОЗВОЛЯЮЩИЙ ПО ИНДЕКСУ 0 ОБРАЩАТЬСЯ К ПОЛЮ r:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 13);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "ОПЕРАЦИЯ БИНАРНОГО +:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(407, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(339, 13);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "ИНДЕКСАТОР, ПОЗВОЛЯЮЩИЙ ПО ИНДЕКСУ 1 – К ПОЛЮ text:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(34, 283);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(224, 57);
+            this.textBox9.TabIndex = 37;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(68, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(140, 13);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "ОПЕРАЦИЯ УНАРНОГО -:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(784, 529);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Задание 12";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- Task 12 Forms/Form1.cs
+++ Task 12 Forms/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Task_12_Forms
 {
@@ -15,6 +16,160 @@ namespace Task_12_Forms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        class Regular
+        {
+            public Regular(string pattern, string txt)
+            {
+                r = new Regex(pattern);
+                text = txt;
+            }
+            private Regex r;
+            private string text;
+
+            public string Match_patter()
+            {
+                string s = "";
+                MatchCollection m = r.Matches(text);
+                foreach (Match x in m)
+                {
+                    s = "Содержит";
+                    return s;
+                }
+                s = "Не содержит";
+                return s;
+            }
+
+            public string Output_on_display()
+            {
+                string s = "";
+                MatchCollection m = r.Matches(text);
+                foreach (Match x in m)
+                    s += x.Value;
+                return s;
+            }
+
+            public string Delete()
+            {
+                MatchCollection m = r.Matches(text);
+                string s = text;
+                foreach (Match x in m)
+                {
+                    int i = s.IndexOf(x.Value);
+                    int l = x.Value.Length;
+                    s = s.Remove(i, l);
+                }
+                return s;
+            }
+
+            public string Text
+            {
+                get { return text; }
+                set { text = value; }
+            }
+            public Regex R
+            {
+                get { return r; }
+                set { r = value; }
+            }
+
+            public string this[int k]
+            {
+                get
+                {
+                    if (k == 0)
+                    {
+                        return R.ToString();
+                    }
+                    else if (k == 1)
+                    {
+                        return text;
+                    }
+                    else
+                        return "Некорректный индекс";
+                }
+            }
+            public static Regular operator -(Regular re)
+            {
+                MatchCollection m = re.R.Matches(re.text);
+                string s = re.text;
+                foreach (Match x in m)
+                {
+                    int i = s.IndexOf(x.Value);
+                    int l = x.Value.Length;
+                    s = s.Remove(i, l);
+                }
+                re.text = s;
+                return re;
+            }
+            public static bool operator false(Regular re)
+            {
+                return re.text.Length == 0;
+            }
+            public static bool operator true(Regular re)
+            {
+                return re.text.Length != 0;
+            }
+            public static Regular operator +(Regular re, string s)
+            {
+                re.text = re.text + s;
+                return re;
+            }
+            public override string ToString()
+            {
+                return "Регулярное выражение: " + R + " текст: " + text;
+            }
+            public static Regular StringToRegular(string s)
+            {
+                try
+                {
+                    int a, b;
+                    a = s.IndexOf("[");
+                    b = s.IndexOf("]");
+                    if (a == -1 || b == -1)
+                    {
+                        throw new Exception("Ошибка преобразования");
+                    }
+
+                    string s1 = s.Substring(a + 1, b - a - 1);
+                    string s2 = s.Remove(a, b - a + 1);
+
+                    Regular c = new Regular(s1, s2);
+                    return c;
+                }
+                catch (Exception E)
+                {
+                    Console.WriteLine(E.Message);
+                    return null;
+                }
+            }
+        }
+            
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string text = "Мальчик проснулся в 11:50, а должен был проснуться в 09:00.";
+            textBox1.Text += text;
+            string pattern = ("[0-2][0-9]:[0-6][0-9]");
+            textBox2.Text += pattern;
+            Regular myReg = new Regular(pattern, text);
+            if (myReg)
+            {
+                textBox3.Text += "Строка не пуста";
+            }
+            else
+            {
+                textBox3.Text += "Строка пуста";
+            }
+            textBox4.Text += myReg.ToString();
+            Regular myReg2 = Regular.StringToRegular("[19:56] играю на аккордеоне");
+            textBox5.Text += myReg2.ToString();
+            textBox6.Text += myReg[0];
+            textBox7.Text += myReg[1] + "\n\r";
+            textBox7.Text += "\n\r"+"Другое значение индекса:" +"\n\r";
+            textBox7.Text += myReg[6];
+            textBox8.Text += myReg = myReg + "12345";
+            textBox9.Text += myReg = -myReg;
         }
     }
 }
